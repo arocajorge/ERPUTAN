562b0e603c0788b03224c35fd41030bc855cd6e9
vanessaoh <39768774+vanessaoh@users.noreply.github.com>
2018-06-13 15:18:14 -0500

    m

---
+++ ERP_naturisa/ERP_database/ERP_database/mobileSCI/Tables/tbl_sincronizacion_turno.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [mobileSCI].[tbl_sincronizacion_turno] (
+    [IdSincronizacion] INT      IDENTITY (1, 1) NOT NULL,
+    [EnUso]            BIT      NOT NULL,
+    [FechaInicio]      DATETIME NOT NULL,
+    CONSTRAINT [PK_tbl_sincronizacion_turno] PRIMARY KEY CLUSTERED ([IdSincronizacion] ASC)
+);
+
