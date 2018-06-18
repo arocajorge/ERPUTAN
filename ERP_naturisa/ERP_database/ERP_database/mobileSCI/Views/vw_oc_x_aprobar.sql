a20e49c48f384e270a11cc8ab46f71c6e4484b88
AROCA <AROCA@DESKTOP-V0N0DGJ>
2018-06-17 21:38:27 -0500

    Cambios en vista para aceptar sincronizaciones

--- ERP_naturisa/ERP_database/ERP_database/mobileSCI/Views/vw_oc_x_aprobar.sql
+++ ERP_naturisa/ERP_database/ERP_database/mobileSCI/Views/vw_oc_x_aprobar.sql
@@ -1,9 +1,9 @@
-﻿CREATE VIEW [mobileSCI].[vw_oc_x_aprobar]
+﻿CREATE VIEW mobileSCI.vw_oc_x_aprobar
 AS
 SELECT        ISNULL(ROW_NUMBER() OVER (ORDER BY dbo.com_ordencompra_local_det.IdEmpresa), 0) IdRow, dbo.com_ordencompra_local_det.IdEmpresa, dbo.com_ordencompra_local_det.IdSucursal, 
 dbo.com_ordencompra_local_det.IdOrdenCompra, dbo.com_ordencompra_local_det.Secuencia, dbo.com_ordencompra_local_det.IdProducto, dbo.com_ordencompra_local_det.IdUnidadMedida, dbo.com_ordencompra_local.IdProveedor, 
-isnull(SUM(dbo.com_ordencompra_local_det.do_Cantidad), 0) AS cant_oc, ISNULL(SUM(dbo.in_Ing_Egr_Inven_det.dm_cantidad_sinConversion), 0) AS cant_in, isnull(SUM(dbo.com_ordencompra_local_det.do_Cantidad) 
-- ISNULL(SUM(dbo.in_Ing_Egr_Inven_det.dm_cantidad_sinConversion), 0), 0) AS saldo, dbo.in_Producto.pr_descripcion, dbo.in_Producto.pr_codigo, dbo.in_UnidadMedida.Descripcion, dbo.tb_persona.pe_nombreCompleto, 
+isnull(SUM(dbo.com_ordencompra_local_det.do_Cantidad), 0) AS cant_oc, ISNULL(SUM(dbo.in_Ing_Egr_Inven_det.dm_cantidad_sinConversion), 0) + isnull(sum(mov.cantidad),0)  AS cant_in, isnull(SUM(dbo.com_ordencompra_local_det.do_Cantidad) 
+- ISNULL(SUM(dbo.in_Ing_Egr_Inven_det.dm_cantidad_sinConversion), 0), 0) - isnull(sum(mov.cantidad),0) AS saldo, dbo.in_Producto.pr_descripcion, dbo.in_Producto.pr_codigo, dbo.in_UnidadMedida.Descripcion, dbo.tb_persona.pe_nombreCompleto, 
 dbo.com_ordencompra_local.oc_fecha, dbo.com_ordencompra_local.oc_observacion, dbo.in_Producto.IdUnidadMedida_Consumo
 FROM            dbo.tb_persona INNER JOIN
                          dbo.cp_proveedor INNER JOIN
@@ -21,9 +21,115 @@ FROM            dbo.tb_persona INNER JOIN
                          dbo.in_Ing_Egr_Inven.IdMovi_inven_tipo = dbo.in_Ing_Egr_Inven_det.IdMovi_inven_tipo AND dbo.in_Ing_Egr_Inven.IdNumMovi = dbo.in_Ing_Egr_Inven_det.IdNumMovi ON 
                          dbo.com_ordencompra_local_det.IdEmpresa = dbo.in_Ing_Egr_Inven_det.IdEmpresa_oc AND dbo.com_ordencompra_local_det.IdSucursal = dbo.in_Ing_Egr_Inven_det.IdSucursal_oc AND 
                          dbo.com_ordencompra_local_det.IdOrdenCompra = dbo.in_Ing_Egr_Inven_det.IdOrdenCompra AND dbo.com_ordencompra_local_det.Secuencia = dbo.in_Ing_Egr_Inven_det.Secuencia_oc
+						 LEFT JOIN mobileSCI.tbl_movimientos_det as mov on mov.IdEmpresa_oc = dbo.com_ordencompra_local_det.IdEmpresa and mov.IdSucursal_oc = dbo.com_ordencompra_local_det.IdSucursal
+						 and mov.IdOrdenCompra = dbo.com_ordencompra_local_det.IdOrdenCompra and mov.secuencia_oc = dbo.com_ordencompra_local_det.Secuencia and mov.Aprobado = 0 and mov.Estado = 'A'
 WHERE        (dbo.com_ordencompra_local.IdEstadoAprobacion_cat = 'APRO') AND (dbo.com_ordencompra_local.IdEstado_cierre <> 'CER') AND (dbo.com_ordencompra_local.Estado = 'A') AND 
                          (dbo.com_ordencompra_local.oc_fecha BETWEEN DATEADD(MONTH, - 3, GETDATE()) AND GETDATE())
 GROUP BY dbo.com_ordencompra_local_det.IdEmpresa, dbo.com_ordencompra_local_det.IdSucursal, dbo.com_ordencompra_local_det.IdOrdenCompra, dbo.com_ordencompra_local_det.Secuencia, 
                          dbo.com_ordencompra_local_det.IdProducto, dbo.com_ordencompra_local_det.IdUnidadMedida, dbo.com_ordencompra_local.IdProveedor, dbo.in_Producto.pr_descripcion, dbo.in_Producto.pr_codigo, 
                          dbo.in_UnidadMedida.Descripcion, dbo.tb_persona.pe_nombreCompleto, dbo.com_ordencompra_local.oc_fecha, dbo.com_ordencompra_local.oc_observacion, dbo.in_Producto.IdUnidadMedida_Consumo
 HAVING        (ROUND(SUM(dbo.com_ordencompra_local_det.do_Cantidad) - ISNULL(SUM(dbo.in_Ing_Egr_Inven_det.dm_cantidad_sinConversion), 0), 2) > 0)
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'mobileSCI', @level1type = N'VIEW', @level1name = N'vw_oc_x_aprobar';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+', @level0type = N'SCHEMA', @level0name = N'mobileSCI', @level1type = N'VIEW', @level1name = N'vw_oc_x_aprobar';
+
--- ERP_naturisa/ERP_database/ERP_database/mobileSCI/Views/vw_stock.sql
+++ ERP_naturisa/ERP_database/ERP_database/mobileSCI/Views/vw_stock.sql
@@ -1,7 +1,8 @@
 ﻿CREATE VIEW [mobileSCI].[vw_stock]
 AS
 SELECT        dbo.in_Ing_Egr_Inven_det.IdEmpresa, dbo.in_Ing_Egr_Inven_det.IdSucursal, dbo.in_Ing_Egr_Inven_det.IdBodega, dbo.in_Ing_Egr_Inven_det.IdProducto, dbo.in_Producto.pr_codigo AS CodProducto, 
-                         dbo.in_Producto.pr_descripcion AS NomProducto, dbo.in_Producto.IdUnidadMedida_Consumo, isnull(round(SUM(dbo.in_Ing_Egr_Inven_det.dm_cantidad),2),0) AS Stock, dbo.in_UnidadMedida.Descripcion AS NomUnidadMedida
+                         dbo.in_Producto.pr_descripcion AS NomProducto, dbo.in_Producto.IdUnidadMedida_Consumo, ISNULL(ROUND(SUM(dbo.in_Ing_Egr_Inven_det.dm_cantidad), 2), 0) + ISNULL(SUM(mov.cantidad), 0) AS Stock, 
+                         dbo.in_UnidadMedida.Descripcion AS NomUnidadMedida
 FROM            mobileSCI.tbl_bodega INNER JOIN
                          dbo.in_Ing_Egr_Inven_det ON mobileSCI.tbl_bodega.IdEmpresa = dbo.in_Ing_Egr_Inven_det.IdEmpresa AND mobileSCI.tbl_bodega.IdSucursal = dbo.in_Ing_Egr_Inven_det.IdSucursal AND 
                          mobileSCI.tbl_bodega.IdBodega = dbo.in_Ing_Egr_Inven_det.IdBodega INNER JOIN
@@ -15,7 +16,15 @@ FROM            mobileSCI.tbl_bodega INNER JOIN
                          dbo.in_movi_inven_tipo ON dbo.in_Ing_Egr_Inven_det.IdEmpresa = dbo.in_movi_inven_tipo.IdEmpresa AND dbo.in_Ing_Egr_Inven_det.IdMovi_inven_tipo = dbo.in_movi_inven_tipo.IdMovi_inven_tipo INNER JOIN
                          dbo.in_Producto ON dbo.in_Ing_Egr_Inven_det.IdEmpresa = dbo.in_Producto.IdEmpresa AND dbo.in_Ing_Egr_Inven_det.IdProducto = dbo.in_Producto.IdProducto AND 
                          dbo.in_Ing_Egr_Inven_det.IdEmpresa = dbo.in_Producto.IdEmpresa AND dbo.in_Ing_Egr_Inven_det.IdProducto = dbo.in_Producto.IdProducto INNER JOIN
-                         dbo.in_UnidadMedida ON dbo.in_Producto.IdUnidadMedida_Consumo = dbo.in_UnidadMedida.IdUnidadMedida
+                         dbo.in_UnidadMedida ON dbo.in_Producto.IdUnidadMedida_Consumo = dbo.in_UnidadMedida.IdUnidadMedida LEFT OUTER JOIN
+                         (SELECT        mobileSCI.tbl_movimientos_det.IdEmpresa,mobileSCI.tbl_movimientos_det.IdSucursal, mobileSCI.tbl_movimientos_det.IdBodega, mobileSCI.tbl_movimientos_det.IdProducto, mobileSCI.tbl_movimientos_det.cantidad * dbo.in_UnidadMedida_Equiv_conversion.valor_equiv as cantidad
+							FROM            mobileSCI.tbl_movimientos_det INNER JOIN
+											dbo.in_Producto ON mobileSCI.tbl_movimientos_det.IdEmpresa = dbo.in_Producto.IdEmpresa AND mobileSCI.tbl_movimientos_det.IdProducto = dbo.in_Producto.IdProducto INNER JOIN
+											dbo.in_UnidadMedida_Equiv_conversion ON mobileSCI.tbl_movimientos_det.IdUnidadMedida = dbo.in_UnidadMedida_Equiv_conversion.IdUnidadMedida AND 
+											dbo.in_Producto.IdUnidadMedida_Consumo = dbo.in_UnidadMedida_Equiv_conversion.IdUnidadMedida_equiva
+							where mobileSCI.tbl_movimientos_det.Estado = 'A' and mobileSCI.tbl_movimientos_det.Aprobado = 0) AS mov 
+							ON mov.IdEmpresa = dbo.in_Ing_Egr_Inven_det.IdEmpresa AND mov.IdSucursal = dbo.in_Ing_Egr_Inven_det.IdSucursal AND dbo.in_Ing_Egr_Inven_det.IdBodega = mov.IdBodega AND 
+                         dbo.in_Ing_Egr_Inven_det.IdProducto = mov.IdProducto 
 WHERE        (dbo.in_Motivo_Inven.Genera_Movi_Inven = 'S') AND (dbo.in_movi_inven_tipo.Genera_Movi_Inven = 1) AND (dbo.in_Ing_Egr_Inven.Estado = 'A')
 GROUP BY dbo.in_Ing_Egr_Inven_det.IdEmpresa, dbo.in_Ing_Egr_Inven_det.IdSucursal, dbo.in_Ing_Egr_Inven_det.IdBodega, dbo.in_Ing_Egr_Inven_det.IdProducto, dbo.in_Producto.pr_codigo, dbo.in_Producto.pr_descripcion, 
                          dbo.in_Producto.IdUnidadMedida_Consumo, dbo.in_UnidadMedida.Descripcion
