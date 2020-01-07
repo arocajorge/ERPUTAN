0b0323e91389ad188a1e1281a81f2335f65005c0
WendyCherres <wendycherres@gmail.com>
2020-01-07 10:06:05 -0500

    ...

--- ERP_naturisa/ERP/Core.Erp.Reportes/Inventario/XINV_Rpt029_Bus.cs
+++ ERP_naturisa/ERP/Core.Erp.Reportes/Inventario/XINV_Rpt029_Bus.cs
@@ -37,5 +37,17 @@ namespace Core.Erp.Reportes.Inventario
         {
 
         }
+
+        public List<XINV_Rpt029_Info> Get_list(int IdEmpresa, int IdSucursal, int IdBodega, decimal IdProducto, DateTime fecha_corte)
+        {
+            try
+            {
+                return Odata.Get_list(IdEmpresa, IdSucursal, IdBodega, IdProducto, fecha_corte);
+            }
+            catch (Exception)
+            {
+                return new List<XINV_Rpt029_Info>();
+            }
+        }
     }
 }
--- ERP_naturisa/ERP/Core.Erp.Reportes/Inventario/XINV_Rpt029_Info.cs
+++ ERP_naturisa/ERP/Core.Erp.Reportes/Inventario/XINV_Rpt029_Info.cs
@@ -28,5 +28,32 @@ namespace Core.Erp.Reportes.Inventario
         public string nom_UnidadMedida { get; set; }
 
 
+        //public int IdEmpresa { get; set; }
+        public int IdSucursalOrigen { get; set; }
+        public int IdBodegaOrigen { get; set; }
+        public decimal IdTransferencia { get; set; }
+        public int dt_secuencia { get; set; }
+        //public Nullable<decimal> IdProducto { get; set; }
+        //public string pr_descripcion { get; set; }
+        public double dt_cantidad { get; set; }
+        public string IdEstablecimiento { get; set; }
+        public string IdPuntoEmision { get; set; }
+        public string NumDocumento_Guia { get; set; }
+        public string NumeroAutorizacion { get; set; }
+        public Nullable<System.DateTime> FechaAutorizacion { get; set; }
+        public string IdentificacionTransportista { get; set; }
+        public string NombreTransportista { get; set; }
+        public string MotivoGuia { get; set; }
+        public string Direc_sucu_Llegada { get; set; }
+        public string Direc_sucu_Partida { get; set; }
+        public Nullable<System.DateTime> Fecha { get; set; }
+        public string Nombre { get; set; }
+        public string NombreDestinatario { get; set; }
+        public string IdentificacionDestinatario { get; set; }
+        public string Su_Descripcion { get; set; }
+        public string Su_Direccion { get; set; }
+        public string NombreEmpresa { get; set; }
+        public string NumeroContribuyente { get; set; }
+        public string em_ruc { get; set; }
     }
 }
